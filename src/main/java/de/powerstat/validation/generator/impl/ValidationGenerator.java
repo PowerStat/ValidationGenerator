9f46b65173212210c77c2770e54337fe237d5dbf
Kai Hofmann <powerstat@web.de>
2021-02-23 18:37:04 +0100

    Fixed checkstyle doctype, pmd ruleset xsd and eclipse settings. Code improvements.

--- src/main/java/de/powerstat/validation/generator/impl/ValidationGenerator.java
+++ src/main/java/de/powerstat/validation/generator/impl/ValidationGenerator.java
@@ -43,6 +43,17 @@ public final class ValidationGenerator
   private static final String TESTS = "TESTS"; //$NON-NLS-1$
 
   /**
+   * Package path.
+   */
+  private static final String PACKAGE_PATH = "de/powerstat/validation/values"; //$NON-NLS-1$
+
+  /**
+   * Resources path.
+   */
+  private static final String RESOURCES_PATH = "src/main/resources"; //$NON-NLS-1$
+
+
+  /**
    * Template types.
    */
   private static final Set<String> TEMPLATE_TYPES = new HashSet<>();
@@ -56,6 +67,11 @@ public final class ValidationGenerator
    * Class name.
    */
   private final String className;
+
+  /**
+   * Field name.
+   */
+  private final String fieldName;
 
   /**
    * Template type: string, long.
@@ -88,11 +104,20 @@ public final class ValidationGenerator
     Objects.requireNonNull(templateType, "templateType"); //$NON-NLS-1$
     if (!ValidationGenerator.TEMPLATE_TYPES.contains(templateType))
      {
-      throw new IllegalArgumentException("Unknown template type");
+      throw new IllegalArgumentException("Unknown templateType"); //$NON-NLS-1$
      }
-    this.outputPath = outputPath; // TODO verify
-    this.className = className; // TODO verify
     this.templateType = templateType;
+    if (!new File(outputPath).isDirectory())
+     {
+      throw new IllegalArgumentException("outputPath is not a directory"); //$NON-NLS-1$
+     }
+    this.outputPath = outputPath;
+    if (!className.matches("^[A-Z][a-zA-Z0-9_]*$")) //$NON-NLS-1$
+     {
+      throw new IllegalArgumentException("className does not match [A-Z][a-zA-Z0-9_]*"); //$NON-NLS-1$
+     }
+    this.className = className;
+    this.fieldName = className.substring(0, 1).toLowerCase(Locale.getDefault()) + className.substring(1);
    }
 
 
@@ -104,19 +129,18 @@ public final class ValidationGenerator
   public void getClasses() throws IOException
    {
     final TemplateEngine templClass = new TemplateEngine(HandleUndefined.KEEP);
-    templClass.setFile(CLASS, new File("src/main/resources", this.templateType + "Class.tmpl")); //$NON-NLS-1$ //$NON-NLS-2$
+    templClass.setFile(CLASS, new File(RESOURCES_PATH, this.templateType + "Class.tmpl")); //$NON-NLS-1$
     templClass.setVar("CLASSNAME", this.className); //$NON-NLS-1$
-    final String fieldname = this.className.substring(0, 1).toLowerCase(Locale.getDefault()) + this.className.substring(1);
-    templClass.setVar("FIELDNAME", fieldname); //$NON-NLS-1$
+    templClass.setVar("FIELDNAME", this.fieldName); //$NON-NLS-1$
     templClass.parse(CLASS, CLASS);
 
     final TemplateEngine templTests = new TemplateEngine(HandleUndefined.KEEP);
-    templTests.setFile(TESTS, new File("src/main/resources", this.templateType + "Tests.tmpl")); //$NON-NLS-1$ //$NON-NLS-2$
+    templTests.setFile(TESTS, new File(RESOURCES_PATH, this.templateType + "Tests.tmpl")); //$NON-NLS-1$
     templTests.setVar("CLASSNAME", this.className); //$NON-NLS-1$
-    templTests.setVar("FIELDNAME", fieldname); //$NON-NLS-1$
+    templTests.setVar("FIELDNAME", this.fieldName); //$NON-NLS-1$
     templTests.parse(TESTS, TESTS);
 
-    final File dirClass = new File(this.outputPath, "de/powerstat/validation/values"); //$NON-NLS-1$
+    final File dirClass = new File(this.outputPath, PACKAGE_PATH);
     boolean result = dirClass.exists() || dirClass.mkdirs();
     if (result)
      {
@@ -128,10 +152,10 @@ public final class ValidationGenerator
      }
     else
      {
-      LOGGER.error("Could not create directory: " + this.outputPath + "/de/powerstat/validation/values"); //$NON-NLS-1$ //$NON-NLS-2$
+      LOGGER.error("Could not create directory: {}/{}", this.outputPath, PACKAGE_PATH); //$NON-NLS-1$
      }
 
-    final File dirTests = new File(this.outputPath, "de/powerstat/validation/values/test"); //$NON-NLS-1$
+    final File dirTests = new File(this.outputPath, PACKAGE_PATH + "/test"); //$NON-NLS-1$
     result = dirTests.exists() || dirTests.mkdirs();
     if (result)
      {
@@ -143,7 +167,7 @@ public final class ValidationGenerator
      }
     else
      {
-      LOGGER.error("Could not create directory: " + this.outputPath + "/de/powerstat/validation/values/test"); //$NON-NLS-1$ //$NON-NLS-2$
+      LOGGER.error("Could not create directory: {}/{}/test", this.outputPath, PACKAGE_PATH); //$NON-NLS-1$
      }
 
    }
